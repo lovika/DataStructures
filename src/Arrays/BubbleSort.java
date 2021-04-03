f3e28ef5442a86dd80b45c89efb21fbe920025ca
lovika <lovikagrover1996@gmail.com>
2021-04-03 13:01:38 +0530

    Avoid modifying arr[] passed in BubbleSort-sortArray function - https://bit.ly/3wr4qUX

--- src/Arrays/BubbleSort.java
+++ src/Arrays/BubbleSort.java
@@ -2,32 +2,36 @@ package Arrays;
 
 /**
  * Bubble Sort
+ * Test Cases Verified on gfg (https://bit.ly/3wrdHN4)
  * Author : Lovika Grover
  */
 public class BubbleSort {
 
     public int[] sortArray(int arr[]) {
         int len = arr.length;
+        int[] sortedArr = new int[len];
+
+        System.arraycopy(arr, 0, sortedArr, 0, len);
+
         for(int i = 0; i < len; i++) {
             for(int j = 0; j < len - i - 1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                if (sortedArr[j] > sortedArr[j+1]) {
+                    int temp = sortedArr[j];
+                    sortedArr[j] = sortedArr[j+1];
+                    sortedArr[j+1] = temp;
                 }
             }
         }
-        return arr;
+        return sortedArr;
     }
 
     public static void main(String[] args) {
-
-        int arr[] = {4, 5, 1, 8, 3, 9, 6, 6};
+        int[] arr = {4, 5, 1, 8, 3, 9, 6, 6};
 
         BubbleSort bubbleSort = new BubbleSort();
-        int sortedArr[] = bubbleSort.sortArray(arr);
-        for (int i = 0 ; i < sortedArr.length; i++)
-            System.out.println(sortedArr[i]);
+        int[] sortedArr = bubbleSort.sortArray(arr);
+        for (int j : sortedArr)
+            System.out.println(j);
 
     }
 }
