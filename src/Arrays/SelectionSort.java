1b59a5c4381047e7fcd8475f40b7ed75bcc26f6c
lovika <lovikagrover1996@gmail.com>
2021-04-03 17:16:43 +0530

    Selection Sort

--- src/Arrays/SelectionSort.java
+++ src/Arrays/SelectionSort.java
@@ -1,2 +1,38 @@
-package Arrays;public class SelectedSort {
+package Arrays;
+
+/**
+ * Selection Sort
+ * Test Cases Verified on gfg (https://practice.geeksforgeeks.org/problems/selection-sort/1)
+ * Author : Lovika Grover
+ */
+public class SelectionSort {
+
+    public int[] sortArray(int[] arr) {
+        int len = arr.length;
+        int[] sortedArr = new int[len];
+        System.arraycopy(arr, 0, sortedArr, 0, len);
+        for (int i = 0; i < len; i++) {
+            int min = i;
+            for (int j = i; j < len; j++) {
+                if (sortedArr[j] < sortedArr[min]) {
+                    min = j;
+                }
+            }
+            int temp = sortedArr[i];
+            sortedArr[i] = sortedArr[min];
+            sortedArr[min] = temp;
+        }
+        return sortedArr;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 1, 8, 3, 9, 6, 6};
+
+        SelectionSort selectionSort = new SelectionSort();
+        int[] sortedArr = selectionSort.sortArray(arr);
+        for (int i : sortedArr) {
+            System.out.println(i);
+        }
+    }
 }
