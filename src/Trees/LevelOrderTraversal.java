c453e51ebb214cda23b79560afd9efb5e1f91009
lovika <lovikagrover1996@gmail.com>
2021-05-01 16:36:56 +0530

    Level Order Traversal in Binary Tree

--- src/Trees/LevelOrderTraversal.java
+++ src/Trees/LevelOrderTraversal.java
@@ -1,2 +1,33 @@
-package Trees;public class LevelOrderTraversal {
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Print Level Order Traversal
+ * Tested on : https://practice.geeksforgeeks.org/problems/level-order-traversal/1
+ * Author : Lovika
+ */
+public class LevelOrderTraversal
+{
+    //Function to return the level order traversal of a tree.
+    static ArrayList<Integer> levelOrder(Node node)
+    {
+        ArrayList<Integer> levelOrder = new ArrayList<>();
+        // Your code here
+        Queue<Node> storeLevelOrder = new LinkedList<>();
+        storeLevelOrder.add(node);
+        while (!storeLevelOrder.isEmpty()) {
+            Node first = storeLevelOrder.remove();
+            levelOrder.add(first.data);
+            if (first.left != null) {
+                storeLevelOrder.add(first.left);
+            }
+            if (first.right != null) {
+                storeLevelOrder.add(first.right);
+            }
+        }
+        return levelOrder;
+    }
 }
